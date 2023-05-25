372ec191c79591be4d50a50a03414d92c09d26dc
bballet <bertrand.ballet@agriculture.gouv.fr>
2023-05-25 14:15:42 +0000

    connexion au serveur postgre du SSP-Cloud complétion du script pour découper les parcelles du rpg bug avec le write_sf

--- Sujet2-Etape1.R
+++ Sujet2-Etape1.R
@@ -10,30 +10,36 @@
 # références         : https://github.com/InseeFrLab/funathon2023_sujet2 
 
 library(tidyverse); 
-library(janitor);
+library(aws.s3)
 library(sf)
-library(RPostgreSQL)
-library(ggplot2)
-library(cowplot)
-library(maptiles)
-library(leaflet)
-library(htmltools)
-library(htmlwidgets)
-library(tictoc)
-library(openxlsx)
+library(RPostgres)
 
+# library(janitor);
+# library(ggplot2)
+# library(cowplot)
+# library(maptiles)
+# library(leaflet)
+# library(htmltools)
+# library(htmlwidgets)
+# library(tictoc)
+# library(openxlsx)
 
+# ajout
+ 
 # 1 - Créer une table avec un point en récupérant les coordonnées GPS --------
 
 lat<- 43.44763763593564
 lon<- 1.2887755354488053
 rayon<-5000
 
+# janitor (clean_names) non disponible (?)
 point<-data.frame(lon,lat,rayon) %>% 
   st_as_sf(coords = c("lon","lat"),crs = "EPSG:4326") %>%
   mutate(coord_pt_gps=st_as_text(geometry)) %>% 
-  st_transform("EPSG:2154") %>% clean_names() %>% st_sf() 
+  st_transform("EPSG:2154") %>% st_sf()
 st_geometry(point)<-"geom"
+
+# %>% clean_names()
 
 st_crs(point)
 st_geometry_type(point)
@@ -42,23 +48,76 @@ st_as_text(st_sfc(point$geom, crs = 4326), EWKT = TRUE)
 
 # 2 - Export de la table point vers PostGis -------------------------------
 
-# define postgres user password
-pw <- "x5D25m2Xpt"
+# le mot de passe est stocké dans un secret Vault
+postgresql_password <- rstudioapi::askForPassword(prompt = "Entrez le password PostgreSQL")
 
-# connection à la base Postgres Grald/rpg
-cnx <- dbConnect(PostgreSQL(),
-                      host = "stats-prod-postgres-49.zsg.agri", # host name, can be website/server
-                      port = "4000", # default port of postgres
-                      dbname = "rpg", # name of database in postgres
-                      user = "dbograld", # the default user
-                      password = pw, # password of user
-                      options="-c search_path=rpg,public" # specify what schema to connect to
-)
+# Connection à PostgreSQL
+cnx <- dbConnect(Postgres(),
+                 user = "projet-funathon",
+                 password = postgresql_password,
+                 host = "postgresql-438832",
+                 dbname = "defaultdb",
+                 port = 5432,
+                 check_interrupts = TRUE,
+                 options="-c search_path=rpg,public", # specify what schema to connect to
+                 application_name = paste(paste0(version[["language"]], " ",
+                                                 version[["major"]], ".",
+                                                 version[["minor"]]),
+                                          tryCatch(basename(rstudioapi::getActiveDocumentContext()[["path"]]),
+                                                   error = function(e) ''),
+                                          sep = " - "))
 
+# suppression de la table «point» si elle existe
+dbSendQuery(cnx,"DROP TABLE IF EXISTS rpg.point CASCADE;")
 
-# write_sf(point, cnx, Id(schema = "rpg", table = "point"), append = T)
+# ecriture de la table dans la base PostGis
 write_sf(point, cnx, append = T)
-# st_write(point,dsn=cnx,layer="point",delete_layer = TRUE)
+
+# ajout d'une clé primaire
+dbSendQuery(cnx,"ALTER TABLE rpg.point ADD CONSTRAINT point_pkey PRIMARY KEY(coord_pt_gps);")
+
+# ajout d'un index
+dbSendQuery(cnx,"CREATE INDEX ON rpg.point USING gist(geom);")
+dbExecute(cnx,"CREATE INDEX ON rpg.point USING gist(geom);")
+
+# 3 - Exécution de la requête de découpage du RPG autour du point sur PostGis  -------
+
+tic()
+dbSendQuery(cnx,"DROP TABLE IF EXISTS rpg.parc_prox CASCADE;")
+toc()
+
+tic()
+dbSendQuery(cnx,"CREATE TABLE rpg.parc_prox AS
+	SELECT row_number() OVER () AS row_id, p.coord_pt_gps, p.rayon, r.*  
+	FROM rpg.point p, rpg.parcelles r 
+	WHERE ST_DWithin(p.geom,r.geom,p.rayon);")
+toc()
+
+# ajout d'une clé primaire
+tic()
+dbSendQuery(cnx,"ALTER TABLE rpg.parc_prox ADD CONSTRAINT parc_prox_pk PRIMARY KEY(gid);")
+toc()
+
+# ajout d'un index
+tic()
+dbSendQuery(cnx,"CREATE INDEX parc_prox_geom_idx ON rpg.parc_prox USING gist(geom);")
+toc()
+
+
+# 4 - Téléchargement des parcelles proches sous R-------------------------------
+
+# parc_prox<-read_sf(cnx,parc_prox)
+
+parc_prox<-st_read(cnx, query="select * from rpg.parc_prox;")
+
+plot(st_geometry(parc_prox))
+
+parc_prox %>% 
+  summary(parc_prox$code_cultu)
+
+summary(parc_prox$surface)
+describe(parc_prox$code_cultu)
+
 
 
 
